d0e805dd4be2326b723dca7cb46a2fb7d60e63b9
eotbdata <eotbdata.dnr@maryland.gov>
2026-09-15 15:19:46 -0400

    260915 final logo size update

--- SAV-App/app.R
+++ SAV-App/app.R
@@ -81,7 +81,7 @@ ui <- fluidPage(
   # # Theme, title and DNR banner
   theme = bs_theme(preset = "flatly"),
   tags$div(
-    style = "display: flex; justify-content: space-between; align-items: center; margin-top: 20px; margin-bottom: 20px;",
+    style = "display: flex; justify-content: space-between; align-items: center; margin-top: 20px; margin-bottom: 30px;",
     tags$h3(("MD DNR's Submerged Aquatic Vegetation (SAV) Coverage"), style = "margin: 0;"),
     tags$img(src = "DNR_logo_final.png", height = "20px")
   ),
